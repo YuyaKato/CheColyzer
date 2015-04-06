c5271370bcdbb48527b347d90b13adc414268867
YuyaKato <kato@sakailab.info>
2015-04-06 18:39:40 +0900

    総ログイン時間，各種取り込み回数，プログレスインディケータの表示

--- CHAnalyzer/src/ca/app/CAApplication.java
+++ CHAnalyzer/src/ca/app/CAApplication.java
@@ -1,13 +1,20 @@
 package ca.app;
 
 import java.io.File;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
+import ca.system.CALogReduct;
 import ca.view.CAFileChooser;
 import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -15,6 +22,8 @@ import javafx.stage.Stage;
 public class CAApplication extends Application{
 	
 	public static final String APP_NAME = "CheColyzer";
+	
+	private CALogReduct lr = new CALogReduct();
 
 	public static void main(String[] args) {
 		launch(args);
@@ -33,7 +42,12 @@ public class CAApplication extends Application{
 		VBox vbox = new VBox();
 		StackPane root = new StackPane();
 		Scene scene;
-		
+		Label tltLabel = new Label();
+		Label aicLabel = new Label();
+		Label jicLabel = new Label();
+		Label micLabel = new Label();
+		ProgressIndicator indicator = new ProgressIndicator();
+
 		fileSelectButton.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -42,23 +56,48 @@ public class CAApplication extends Application{
 				CAFileChooser fc = new CAFileChooser();
 				File file = fc.open();
 				if (file != null) {
-					readFile(file);
+					indicator.setVisible(true);
+					Task<Void> task = new Task<Void>() {
+
+						@Override
+						protected Void call() throws Exception {
+							lr = reduct(file);
+							return null;
+						}
+					};
+					task.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+
+						@Override
+						public void handle(WorkerStateEvent event) {
+							indicator.setVisible(false);
+							tltLabel.setText("Total Login     : " + lr.getTotalLoginTime() + " minutes");
+							aicLabel.setText("All Import      : " + lr.getAllImportCount() + " times");
+							jicLabel.setText("Java Import     : " + lr.getJavaImportCount() + " times");
+							micLabel.setText("Resource Import : " + lr.getMaterialImportCount() + " times");
+						}
+					});
+					
+					Executor executor = Executors.newSingleThreadExecutor();
+					executor.execute(task);
 				}
 			}
 		});
 		
-		vbox.getChildren().addAll(fileSelectButton);
+		vbox.getChildren().addAll(fileSelectButton, indicator, tltLabel, aicLabel, jicLabel, micLabel);
 		root.getChildren().addAll(vbox);
 		scene = new Scene(root, 300, 250);
 		primaryStage.setTitle(APP_NAME);
 		primaryStage.setScene(scene);
+		indicator.setVisible(false);
+	}
+	
+	public CALogReduct reduct(File file) {
+		CALogReduct lr = new CALogReduct();
+		lr.reduct(lr.loadCSV(file));
+		return lr;
 	}
 	
 	public void open(Stage primaryStage) {
 		primaryStage.show();
-	}
-	
-	public void readFile(File file) {
-		
 	}
 }
--- CHAnalyzer/src/ca/system/CALogReduct.java
+++ CHAnalyzer/src/ca/system/CALogReduct.java
@@ -11,7 +11,15 @@ import clib.common.filesystem.CFileSystem;
 import clib.common.table.CCSVFileIO;
 
 public class CALogReduct {
+	
+	private int totalLoginTime = 0;
+	private int allImportCount = 0;
+	private int javaImportCount = 0;
+	private int materialImportCount = 0;
 
+	/************************
+	 * CSVファイルのロード（リスト形式）
+	 ************************/
 	public List<List<String>> loadCSV(File file) {
 		
 		List<List<String>> table = new ArrayList<List<String>>();
@@ -19,26 +27,46 @@ public class CALogReduct {
 		return table;
 	}
 	
-	public void calcTotalLoginTime(List<List<String>> table) {
+	/******************
+	 * 総ログイン時間の計算
+	 ******************/
+	public void reduct(List<List<String>> table) {
 		
 		boolean login = false;
 		String startTime = "";
 		String endTime;
-		double loginTime = 0;
+		double totalLoginTime = 0;
+		int allImportCount = 0;
+		int javaImportCount = 0;
+		int materialImportCount = 0;
 		
 		for (List<String> cells : table) {
-			if (cells.get(1).equals("login")) {
+			String command = cells.get(1);
+			if (command.equals("login")) {
 				startTime = cells.get(0);
 				login = true;
-			} else if (cells.get(1).equals("logout") && login) {
+			} else if (command.equals("logout") && login) {
 				endTime = cells.get(0);
-				loginTime += calcTime(startTime, endTime);
+				totalLoginTime += calcTime(startTime, endTime);
 				login = false;
+			} else if (command.equals("copy all file")) {
+				allImportCount++;
+			} else if (command.equals("copy java file")) {
+				javaImportCount++;
+			} else if (command.equals("copy material fila")){
+				materialImportCount++;
 			}
 		}
-		System.out.println(loginTime/60);
+		
+		setTotalLoginTime((int) Math.round(totalLoginTime)/60);
+		setAllImportCount(allImportCount);
+		setJavaImportCount(javaImportCount);
+		setMaterialImportCount(materialImportCount);
 	}
 	
+	/*************************
+	 * 開始時刻と終了時刻の差を計算
+	 *************************/
 	public long calcTime(String startTime, String endTime) {
 		
 		try {
@@ -53,9 +81,41 @@ public class CALogReduct {
 		return -1;
 	}
 	
+	public int getTotalLoginTime() {
+		return totalLoginTime;
+	}
+
+	public void setTotalLoginTime(int totalLoginTime) {
+		this.totalLoginTime = totalLoginTime;
+	}
+
+	public int getAllImportCount() {
+		return allImportCount;
+	}
+
+	public void setAllImportCount(int allImportCount) {
+		this.allImportCount = allImportCount;
+	}
+
+	public int getJavaImportCount() {
+		return javaImportCount;
+	}
+
+	public void setJavaImportCount(int javaImportCount) {
+		this.javaImportCount = javaImportCount;
+	}
+
+	public int getMaterialImportCount() {
+		return materialImportCount;
+	}
+
+	public void setMaterialImportCount(int materialImportCount) {
+		this.materialImportCount = materialImportCount;
+	}
+
 	public static void main(String[] args) {
 		CALogReduct lr = new CALogReduct();
 		File file = new File("test/70411069_CHLog.csv");
-		lr.calcTotalLoginTime(lr.loadCSV(file));
+		lr.reduct(lr.loadCSV(file));
 	}
 }
